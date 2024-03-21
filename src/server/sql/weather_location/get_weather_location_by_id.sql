c274e066df43210a52aeb7997792015592cd48a6
Brian Palomar <104478722+9brian@users.noreply.github.com>
2024-03-21 10:44:36 -0700

    Edit get query

--- src/server/sql/weather_location/get_weather_location_by_id.sql
+++ src/server/sql/weather_location/get_weather_location_by_id.sql
@@ -2,4 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-SELECT * FROM weather_location WHERE id=${weather_location_id};
+SELECT * FROM weather_location WHERE id=${id};
