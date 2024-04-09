08e22c4a643f3957dc98f4710134a1b1d6435284
Brian Palomar <104478722+9brian@users.noreply.github.com>
2024-04-09 10:16:32 -0700

    Create static createTable call

--- src/server/models/WeatherData.js
+++ src/server/models/WeatherData.js
@@ -8,6 +8,29 @@
  
  // TODO: WeatherData Class
  class WeatherData {
+    /**
+     * @param id This weather data's id
+     * @param weather_location_id The weather location id
+     * @param start_time The weather data's start_time
+     * @param end_time The weather data's end_time
+     * @param temperature The actual weather data (temperature)
+     */
+    constructor(id, weather_location_id, start_time, end_time, temperature){
+        this.id = id;
+        this.weather_location_id = weather_location_id;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.temperature = temperature;
+    }
+
+    /**
+      * Returns a promise to create the weather data table
+      * @param conn is the connection to use.
+      * @returns {Promise.<>}
+      */
+    static createTable(conn) {
+        return conn.none(sqlFile('weather_data/create_weather_data_table.sql'));
+    }
     
  }
  
